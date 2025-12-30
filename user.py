135a81d39fcca8b5283f992fc3730bccb69f97ae
Ananya-source0510 <ananya.acharya0510@gmail.com>
2025-12-30 21:30:43 +0530

    Implement user management functions in user.py

---
+++ user.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(
+        email=user.email,
+        name=user.name
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def delete_user(db: Session, user_id: int):
+    user = get_user(db, user_id)
+    if user:
+        db.delete(user)
+        db.commit()
+    return user
