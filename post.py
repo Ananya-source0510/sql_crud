5153086e8b9b3f8fa4316527daae4be2837f089c
Ananya-source0510 <ananya.acharya0510@gmail.com>
2025-12-30 21:31:08 +0530

    Implement create and get posts functions

---
+++ post.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+def create_post(db: Session, post: schemas.PostCreate, user_id: int):
+    db_post = models.Post(
+        title=post.title,
+        content=post.content,
+        owner_id=user_id
+    )
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+def get_posts(db: Session):
+    return db.query(models.Post).all()
